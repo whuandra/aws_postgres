de22fef5b5ed96d98b22b38a8a119a17ce3d5ee2
whuandra <winoto@hotmail.com>
2021-06-27 20:50:35 -0700

    test

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "postgresql_database" "my_db" {
+  name              = "my_db"
+  owner             = "my_role"
+  template          = "template0"
+  lc_collate        = "C"
+  connection_limit  = -1
+  allow_connections = true
+}
