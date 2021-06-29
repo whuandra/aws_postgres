88d886e1c5b87e7244c41f980210606c1b415b16
whuandra <winoto@hotmail.com>
2021-06-28 21:04:19 -0700

    Co-authored-by: whuandra <winoto@hotmail.com>

--- main.tf
+++ main.tf
@@ -3,11 +3,31 @@ provider "aws" {
     
 }
 
-resource "postgresql_database" "my_db" {
-  name              = "my_db"
-  owner             = "my_role"
-  template          = "template0"
-  lc_collate        = "C"
-  connection_limit  = -1
-  allow_connections = true
+resource "aws_db_instance" "postgresql" {
+  allocated_storage               = var.allocated_storage
+  engine                          = "postgres"
+  engine_version                  = var.engine_version
+  identifier                      = var.database_identifier
+  snapshot_identifier             = var.snapshot_identifier
+  instance_class                  = var.instance_type
+  storage_type                    = var.storage_type
+  iops                            = var.iops
+  name                            = var.database_name
+  password                        = var.database_password
+  username                        = var.database_username
+  copy_tags_to_snapshot           = var.copy_tags_to_snapshot
+  port                            = var.database_port
+  db_subnet_group_name            = var.subnet_group
+  parameter_group_name            = var.parameter_group
+  storage_encrypted               = var.storage_encrypted
+  deletion_protection             = var.deletion_protection
+
+  tags = merge(
+    {
+      Name        = "DatabaseServer",
+      Project     = var.project,
+      Environment = var.environment
+    },
+    var.tags
+  )
 }
